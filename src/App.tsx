b7e486f2e72ea0f78c977cba91d3509c4fd6dd53
Riya Jain <jriya.shi@gmail.com>
2023-07-28 23:45:23 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Outlet,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Footer from "./common/Footer";
+import Navbar from "./common/Navbar";
+import HomePage from "./pages/HomePage/HomePage";
+import { GlobalContextProvider } from "./contexts/globalContext";
+
+export default function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Root />}>
+        <Route index element={<HomePage />} />
+        <Route path="/0" element={<div />} />
+      </Route>
+    )
+  );
+
+  return (
+    <GlobalContextProvider>
+      <RouterProvider router={router} />
+    </GlobalContextProvider>
+  );
+}
+
+function Root() {
+  return (
+    <main className="relative">
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </main>
+  );
+}
