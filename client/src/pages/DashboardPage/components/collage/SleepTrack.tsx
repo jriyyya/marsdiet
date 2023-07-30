b4d80a305cdb6b53ed864d484eef6648c1e49fa9
Riya Jain <jriya.shi@gmail.com>
2023-07-30 18:12:26 +0530

    Add hours button on SleepTracker

--- client/src/pages/DashboardPage/components/collage/SleepTrack.tsx
+++ client/src/pages/DashboardPage/components/collage/SleepTrack.tsx
@@ -14,10 +14,24 @@ export default function SleepTrack() {
     ],
   };
   return (
-    <div className=" bg-gray-400 bg-opacity-25 basis-2/3 rounded-3xl p-4">
+    <div className=" bg-gray-400 bg-opacity-25 basis-2/3 rounded-3xl p-4 flex flex-col gap-y-4">
       <div className="flex flex-col items-center">
         <ChartComponent data={data} type="line" />
         <div className="text-sm">Month of July</div>
+      </div>
+      <div className="flex flex-col">
+        <p className="text-sm">Input the number of Hours you slept today</p>
+        <div className="flex gap-x-2">
+          <input
+            placeholder="e.g. 4 Hours"
+            type="number"
+            className="py-2 px-4 outline-none border-[#00bfff] border-2 bg-transparent text-[##00bfff] rounded-xl w-full"
+          />
+          <button className="whitespace-nowrap bg-[#00bfff] text-white px-4 rounded-xl">
+            {" "}
+            Add +
+          </button>
+        </div>
       </div>
     </div>
   );
